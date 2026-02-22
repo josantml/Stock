022243b5a37c77da00f006e49201aeaea2613ad4
josantml <132955303+josantml@users.noreply.github.com>
2026-02-22 19:14:45 -0300

    Update mantenimiento page.tsx

--- nextjs-dashboard/app/maintenance/page.tsx
+++ nextjs-dashboard/app/maintenance/page.tsx
@@ -4,7 +4,7 @@ export default function MaintenancePage() {
   return (
     <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white flex flex-col">
       <section className="flex-grow max-w-6xl mx-auto px-4 py-20 text-center w-full">
-         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+         <div className="flex flex-col items-center justify-center min-h-screen">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">Sitio en mantenimiento</h1>
           <p className="text-lg text-gray-600 mb-12 max-w-2xl">Estamos realizando tareas de mantenimiento. Por favor, vuelve más tarde.</p>
         </div>
