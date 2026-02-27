f7bb0696d108fc1165ae69f879eb0fefd5397258
Jose Milera <joseemilera@gmail.com>
2026-02-27 20:45:18 -0300

    fix: admin login working in production with middleware

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -56,7 +56,6 @@ export async function middleware(request: NextRequest) {
   });
 
   console.log("TOKEN:", token);
-  
   const isAdmin = token?.role === 'admin';
 
   // 🚧 Si está en mantenimiento:
